9c433ee68beb61025de532cc48899d6376dfcbda
Andrey Makarychev <makarychev@reg.ru>
2026-03-12 11:45:15 +0400

    introduce sign in page

---
+++ src/app/auth/sign-in/__tests__/page.spec.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import SignInPage from "../page";
+
+describe("SignInPage", () => {
+    it("renders login, password inputs and submit button", () => {
+        render(<SignInPage />);
+
+        const header = screen.getByRole("heading", {
+            name: /login to your account/i,
+        });
+
+        const description = screen.getByText(
+            /enter your email below to login to your account/i
+        );
+
+        const loginInput = screen.getByLabelText(/login/i);
+        const passwordInput = screen.getByLabelText(/password/i);
+
+        const button = screen.getByRole("button", {
+            name: /sign in/i,
+        });
+
+        expect(header).toBeInTheDocument();
+        expect(description).toBeInTheDocument();
+
+        expect(loginInput).toBeInTheDocument();
+        expect(passwordInput).toBeInTheDocument();
+
+        expect(button).toBeInTheDocument();
+
+    });
+});
